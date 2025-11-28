cdfd0645e9e43c9c785c46982dd8f74ddf0683fc
Aleksandar Ristic <storiaca@gmail.com>
2025-11-28 18:35:35 +0100

    Recursive range

---
+++ 007-recurision/zadatak/recursiveRange.js
@@ -0,0 +1,23 @@
+/**
+ * recursiveRange
+Write a function called recursiveRange which accepts 
+a number and adds up all the numbers from 0 
+to the number passed to the function 
+ */
+// SAMPLE INPUT/OUTPUT
+// recursiveRange(6) // 21
+// recursiveRange(10) // 55 
+
+function recursiveRange(num){
+   let result = 0
+
+    if(num === 1) return 1
+
+    for (let i = num; i > 0; i--) {
+        result += i
+    }
+
+    return result;
+}
+
+recursiveRange(10)
