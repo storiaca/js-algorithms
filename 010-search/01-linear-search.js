c8165719e7303f521cb2202b7423c1ae657c113d
Aleksandar Ristic <storiaca@gmail.com>
2026-07-09 15:23:12 +0200

    His solution

--- 010-search/01-linear-search.js
+++ 010-search/01-linear-search.js
@@ -8,3 +8,14 @@ function linearSearch(arr, num) {
 }
 
 linearSearch([4, 6, 7, 89, 23], 9);
+
+// his solution
+
+function linearSearch2(arr, val) {
+  for (let i = 0; i <= arr.length; i++) {
+    if (arr[i] === val) return i;
+  }
+
+  return -1;
+}
+// On
