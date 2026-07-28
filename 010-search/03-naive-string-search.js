71c80a834e83a14d2715f4c77370df257a4ce0fb
Aleksandar Ristic <storiaca@gmail.com>
2026-07-28 13:51:13 +0200

    Naive string search

--- 010-search/03-naive-string-search.js
+++ 010-search/03-naive-string-search.js
@@ -1,3 +1,12 @@
 /**
  * Naive string search
  */
+function naiveSearch(long, short) {
+  for (let i = 0; i < long.length; i++) {
+    for (let j = 0; j < short.length; j++) {
+      console.log(long[i], short[j]);
+    }
+  }
+}
+
+naiveSearch("lorie loled", "lol");
