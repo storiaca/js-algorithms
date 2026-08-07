826ce232fe0dc0ead0bac9a64a3f628db72a3145
Aleksandar Ristic <storiaca@gmail.com>
2026-08-07 20:34:37 +0200

    Compare by len function for strings

--- 011-bubble-sort/01-bubble-sort.js
+++ 011-bubble-sort/01-bubble-sort.js
@@ -7,3 +7,9 @@ function numberCompare(num1, num2) {
 }
 
 [13, 45, 4, 89, 63, 2, 36, 28, 1].sort(numberCompare);
+
+function compareByLen(str1, str2) {
+    return str1.length - str2.length;
+}
+
+["Steele", "Colt", "Data Structures", "Algorithms"].sort(compareByLen)
