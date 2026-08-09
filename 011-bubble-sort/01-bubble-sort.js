96758402f0fcb3d8c42a93cf084b568c41a1f77f
Aleksandar Ristic <storiaca@gmail.com>
2026-08-09 20:29:17 +0200

    Bubble sort start function

--- 011-bubble-sort/01-bubble-sort.js
+++ 011-bubble-sort/01-bubble-sort.js
@@ -2,14 +2,35 @@
  * Bubble sort
  *
  */
-function numberCompare(num1, num2) {
-  return num1 - num2;
+// function numberCompare(num1, num2) {
+//   return num1 - num2;
+// }
+
+// [13, 45, 4, 89, 63, 2, 36, 28, 1].sort(numberCompare);
+
+// function compareByLen(str1, str2) {
+//     return str1.length - str2.length;
+// }
+
+// ["Steele", "Colt", "Data Structures", "Algorithms"].sort(compareByLen)
+
+/**
+ * 
+ * @param [numbers] arr 
+ * @returns Sorted array
+ */
+function bubbleSort(arr) {
+    for (let i = arr.length; i > 0; i--) {
+        for (let j = 0; j < i - 1; j++) {
+            if(arr[j] > arr[j+1]) {
+                // SWAP!
+                let temp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = temp
+            }
+        }
+    }
+    return arr
 }
 
-[13, 45, 4, 89, 63, 2, 36, 28, 1].sort(numberCompare);
-
-function compareByLen(str1, str2) {
-    return str1.length - str2.length;
-}
-
-["Steele", "Colt", "Data Structures", "Algorithms"].sort(compareByLen)
+bubbleSort([37, 45, 29, 8])
