08333c50e400d29578206246169461debadc8160
Aleksandar Ristic <storiaca@gmail.com>
2026-08-10 19:04:36 +0200

    Bubble sort 2nd solution

--- 011-bubble-sort/01-bubble-sort.js
+++ 011-bubble-sort/01-bubble-sort.js
@@ -19,18 +19,33 @@
  * @param [numbers] arr 
  * @returns Sorted array
  */
+// function bubbleSort(arr) {
+//     for (let i = arr.length; i > 0; i--) {
+//         for (let j = 0; j < i - 1; j++) {
+//             console.log(arr, arr[j], arr[j+1])
+//             if(arr[j] > arr[j+1]) {
+//                 // SWAP!
+//                 let temp = arr[j]
+//                 arr[j] = arr[j+1]
+//                 arr[j+1] = temp
+//             }
+//         }
+//         console.log("ONE PASS COMPLETE!")
+//     }
+//     return arr
+// }
+
 function bubbleSort(arr) {
+    const swap = (arr, idx1, idx2) => {
+        [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]
+    }
+
     for (let i = arr.length; i > 0; i--) {
-        for (let j = 0; j < i - 1; j++) {
-            if(arr[j] > arr[j+1]) {
-                // SWAP!
-                let temp = arr[j]
-                arr[j] = arr[j+1]
-                arr[j+1] = temp
-            }
+        for (let j = 0; j < i -1; j++) {
+            swap(arr, j, j + 1)
         }
     }
     return arr
 }
 
-bubbleSort([37, 45, 29, 8])
+bubbleSort([37, 45, 29, 8, 12, 88, -3])
