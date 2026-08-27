084b8e7e3a39ff6c793376b7596cc88f836eae97
Aleksandar Ristic <storiaca@gmail.com>
2026-08-27 20:04:46 +0200

    Insertion sort

---
+++ 013-insertion-sort/insertion-sort.js
@@ -0,0 +1,21 @@
+/**
+ * Insertion sort
+ */
+
+let a = [4, 5, 3, 2, 1, 12, 77, 6, 14];
+
+function insertionSort() {
+  for (let i = 0; i < a.length - 1; i++) {
+    for (let j = i + 1; j > 0; j--) {
+      if (a[j - 1] > a[j]) {
+        let temp = a[j];
+        a[j] = a[j - 1];
+        a[j - 1] = temp;
+      } else break;
+    }
+  }
+  return a;
+}
+
+console.log(insertionSort(a));
+// output   [1, 2, 3, 4, 5, 6, 12, 14, 77]
