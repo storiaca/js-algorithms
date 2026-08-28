26d00b09a0b8f7956eb2c99f738e155dcd951a7d
Aleksandar Ristic <storiaca@gmail.com>
2026-08-28 16:33:00 +0200

    change

--- 013-insertion-sort/insertion-sort.js
+++ 013-insertion-sort/insertion-sort.js
@@ -19,3 +19,13 @@ function insertionSort() {
 
 console.log(insertionSort(a));
 // output   [1, 2, 3, 4, 5, 6, 12, 14, 77]
+function linearSearch(arr, num) {
+  let br = -1;
+  for (let i = 0; i <= arr.length; i++) {
+    if (arr[i] === num) return (br = i);
+  }
+
+  return br;
+}
+
+linearSearch([4, 6, 7, 89, 23], 9);
