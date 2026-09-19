73bcb1bb795379412321d8ff663282ad2dcd1dc3
Aleksandar Ristic <storiaca@gmail.com>
2026-09-19 19:03:23 +0200

    Quick sort finished

--- 016-quick-sort/quick-sort.js
+++ 016-quick-sort/quick-sort.js
@@ -25,3 +25,16 @@ function pivot(arr, start = 0, end = arr.length + 1) {
 }
 
 pivot([4, 8, 2, 1, 5, 7, 6, 3]);
+
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let pivotIndex = pivot(arr, left, right);
+    // left
+    quickSort(arr, left, pivotIndex - 1);
+    // right
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr;
+}
+
+quickSort([4, 6, 9, 1, 2, 5, 3]);
